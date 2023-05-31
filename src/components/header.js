289d3a8d7829fed44a56241ca152ecc90097069b
Whale <whale@Whales-MacBook-Air.local>
2023-05-31 17:30:54 +0700

    check pathname using usePathname

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,29 @@
-import Image from "next/image"
-import { Router } from "next/navigation" 
+"use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export default function Header(){
-    return(
+export default function Header() {
+    const pathName = usePathname();
+    console.log(pathName)
+    return (
         <div className="header relative container bg-white flex justify-between h-28 items-center pt-8">
             <Image src={"/image/logo.svg"} width={238} height={110} alt="" priority></Image>
-            <div className="headerMenu flex gap-20">
-                <span className="">Home</span>
-                <span className="">About</span>
-                <span className="">Service</span>
-                <span className="">Contact</span>
+            <div className="flex gap-20">
+                <Link href="/" className={pathName === "/" ? "headerMenu active" : "headerMenu"}>
+                    Home
+                </Link>
+                <Link href="/about" className={pathName === "/about" ? "headerMenu active" : "headerMenu"}>
+                    About
+                </Link>
+                <Link href="/service" className={pathName === "/service" ? "headerMenu active" : "headerMenu"}>
+                    Service
+                </Link>
+                <Link href="/contact" className={pathName === "/contact" ? "headerMenu active" : "headerMenu"}>
+                    Contact
+                </Link>
             </div>
         </div>
-    )
+    );
 }
