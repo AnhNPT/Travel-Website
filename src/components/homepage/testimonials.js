aa7fcb3b994cb54f4a75f649be647e3149cbd391
Emily <anhnpt2000@gmail.com>
2023-06-11 23:16:42 +0700

    finished slider

--- src/components/homepage/testimonials.js
+++ src/components/homepage/testimonials.js
@@ -1,10 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-// import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
+import Image from "next/image";
 import "swiper/css";
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-// import 'swiper/css/scrollbar';
+
 
 export default function Testimonials() {
     return (
@@ -14,26 +11,61 @@ export default function Testimonials() {
                 <span className="block text-center mt-3 text-1 font-outfit font-semibold text-[60px] leading-[76pxß]">Read our Raving Reviews</span>
                 <span className="block text-center mt-11 text-1 opacity-50 text-xl">Written by real travelers, just like you!</span>
             </div>
-            <Swiper className="mt-20" spaceBetween={38} slidesPerView={3} >
+            <Swiper className="mt-20" loop={true} spaceBetween={38} slidesPerView={3}>
                 <SwiperSlide>
-                    <div className="flex flex-col pt-10 px-8 pb-5 bg-4 rounded-sm border-[2px] border-1 drop-shadow-custom-2">
-                        <span>Liam S.</span>
-                        <span>Hollywood, CA</span>
-                        <span>I booked a custom getaway to Paris (for myself) at great price and the website even helped me book tours and activities. The customer service was excellent and I was reimbursed for lost luggage!</span>
+                    <div className="flex flex-col pt-10 px-8 pb-5 bg-[#FAF9FF] rounded-sm border-[2px] border-1 drop-shadow-custom-2">
+                        <span className="text-2xl font-semibold">Liam S.</span>
+                        <span className="block mt-2 text-base font-medium">Hollywood, CA</span>
+                        <span className="block mt-3 text-base font-medium">I booked a custom getaway to Paris (for myself) at great price and the website even helped me book tours and activities. The customer service was excellent and I was reimbursed for lost luggage!</span>
+                        <div className="flex gap-1 justify-end mt-3">
+                            <Image alt="" src={"/image/Star-blue.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star-blue.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star-blue.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star-blue.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star-blue.svg"} width={25} height={1}></Image>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="text-1 flex flex-col pt-10 px-8 pb-5 bg-4 rounded-sm border-[2px] border-1 drop-shadow-custom-2">
+                        <span className="text-2xl font-semibold">Liam S.</span>
+                        <span className="block mt-2 text-base font-medium">Hollywood, CA</span>
+                        <span className="block mt-3 text-base font-medium">I booked a custom getaway to Paris (for myself) at great price and the website even helped me book tours and activities. The customer service was excellent and I was reimbursed for lost luggage!</span>
+                        <div className="flex gap-1 justify-end mt-3">
+                            <Image alt="" src={"/image/Star.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star.svg"} width={25} height={1}></Image>
+                        </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className="flex flex-col pt-10 px-8 pb-5 bg-[#FAF9FF] rounded-sm border-[2px] border-1 drop-shadow-custom-2">
-                        <span>Liam S.</span>
-                        <span>Hollywood, CA</span>
-                        <span>I booked a custom getaway to Paris (for myself) at great price and the website even helped me book tours and activities. The customer service was excellent and I was reimbursed for lost luggage!</span>
+                        <span className="text-2xl font-semibold">Liam S.</span>
+                        <span className="block mt-2 text-base font-medium">Hollywood, CA</span>
+                        <span className="block mt-3 text-base font-medium">I booked a custom getaway to Paris (for myself) at great price and the website even helped me book tours and activities. The customer service was excellent and I was reimbursed for lost luggage!</span>
+                        <div className="flex gap-1 justify-end mt-3">
+                            <Image alt="" src={"/image/Star-blue.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star-blue.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star-blue.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star-blue.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star-blue.svg"} width={25} height={1}></Image>
+                        </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="flex flex-col pt-10 px-8 pb-5 bg-4 rounded-sm border-[2px] border-1 drop-shadow-custom-2">
-                        <span>Liam S.</span>
-                        <span>Hollywood, CA</span>
-                        <span>I booked a custom getaway to Paris (for myself) at great price and the website even helped me book tours and activities. The customer service was excellent and I was reimbursed for lost luggage!</span>
+                    <div className="text-1 flex flex-col pt-10 px-8 pb-5 bg-4 rounded-sm border-[2px] border-1 drop-shadow-custom-2">
+                        <span className="text-2xl font-semibold">Liam S.</span>
+                        <span className="block mt-2 text-base font-medium">Hollywood, CA</span>
+                        <span className="block mt-3 text-base font-medium">I booked a custom getaway to Paris (for myself) at great price and the website even helped me book tours and activities. The customer service was excellent and I was reimbursed for lost luggage!</span>
+                        <div className="flex gap-1 justify-end mt-3">
+                            <Image alt="" src={"/image/Star.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star.svg"} width={25} height={1}></Image>
+                            <Image alt="" src={"/image/Star.svg"} width={25} height={1}></Image>
+                        </div>
                     </div>
                 </SwiperSlide>
             </Swiper>
